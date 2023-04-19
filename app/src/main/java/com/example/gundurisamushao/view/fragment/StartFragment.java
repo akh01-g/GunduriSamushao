f6ed950bef82f7e2c32a313b4e6fca4000f63b8f
akh01-g <gkharaziani@gmail.com>
2023-04-19 15:28:44 +0400

    Initial commit

---
+++ app/src/main/java/com/example/gundurisamushao/view/fragment/StartFragment.java
@@ -0,0 +1,28 @@
+package com.example.gundurisamushao.view.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.example.gundurisamushao.databinding.FragmentStartBinding;
+
+public class StartFragment extends Fragment {
+
+    private FragmentStartBinding binding;
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = FragmentStartBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
