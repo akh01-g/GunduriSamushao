ff5747a00efa5dbf096342731e2d0ea61914275f
akh01-g <gkharaziani@gmail.com>
2023-04-24 21:40:29 +0400

    initial commit

--- app/src/main/java/com/example/gundurisamushao/view/fragment/ChatFragment.java
+++ app/src/main/java/com/example/gundurisamushao/view/fragment/ChatFragment.java
@@ -11,23 +11,10 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
 import com.example.gundurisamushao.databinding.FragmentChatBinding;
-import com.example.gundurisamushao.model.remote.chat.Message;
 import com.example.gundurisamushao.view.adapter.MessageAdapter;
 import com.example.gundurisamushao.viewmodel.ChatViewModel;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
-
-import java.text.SimpleDateFormat;
-import java.util.ArrayList;
-import java.util.Calendar;
-import java.util.List;
 
 public class ChatFragment extends Fragment {
 
@@ -36,7 +23,6 @@ public class ChatFragment extends Fragment {
     private ChatViewModel chatViewModel = new ChatViewModel();
 
     private FirebaseUser user;
-    DatabaseReference mdb = FirebaseDatabase.getInstance().getReference();
 
     @Nullable
     @Override
