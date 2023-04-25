4741d1cb66ec223c5fd34e1297b26dc9b2d52fc9
akh01-g <gkharaziani@gmail.com>
2023-04-26 00:09:24 +0400

    initial commit

--- app/src/main/java/com/example/gundurisamushao/model/remote/retrofit/RetrofitInstance.java
+++ app/src/main/java/com/example/gundurisamushao/model/remote/retrofit/RetrofitInstance.java
@@ -1,6 +1,5 @@
 package com.example.gundurisamushao.model.remote.retrofit;
 
-import com.example.gundurisamushao.model.remote.brewery.BreweryService;
 import com.example.gundurisamushao.model.remote.crypto.CryptoService;
 
 import hu.akarnokd.rxjava3.retrofit.RxJava3CallAdapterFactory;
@@ -8,19 +7,6 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class RetrofitInstance {
-
-    public static Retrofit getRetrofit(){
-        return new Retrofit.Builder()
-                .baseUrl("https://api.openbrewerydb.org/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-                .build();
-    }
-    public static BreweryService getBreweryService(){
-        return getRetrofit().create(BreweryService.class);
-    }
-
-
 
     public static Retrofit getCryptoRetrofit(){
         return new Retrofit.Builder()
